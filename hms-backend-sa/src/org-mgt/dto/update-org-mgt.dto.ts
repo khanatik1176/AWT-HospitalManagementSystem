ed8d0bf519a5e0d725bff617aa09445803226021
Iftekhar Uddin Mullick <infomullick2000@gmail.com>
2024-05-14 16:16:12 +0600

    Revert all commits on BE - SA for mistake

--- hms-backend-sa/src/org-mgt/dto/update-org-mgt.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrgMgtDto } from './create-org-mgt.dto';
-
-export class UpdateOrgMgtDto extends PartialType(CreateOrgMgtDto) {}
--- hms-backend-sa/src/tenency-auth/dto/update-tenency-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTenencyAuthDto } from './create-tenency-auth.dto';
-
-export class UpdateTenencyAuthDto extends PartialType(CreateTenencyAuthDto) {}
